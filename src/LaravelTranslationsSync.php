1173ff623ce9f391ce2e0cc5139d3e8a8e6f989b
Danique Wijnalda <danique@van-ons.nl>
2025-04-15 15:38:11 +0200

    fix: normalize locale when writing to files

--- src/LaravelTranslationsSync.php
+++ src/LaravelTranslationsSync.php
@@ -35,6 +35,22 @@ class LaravelTranslationsSync
     }
 
     /**
+     * Normalize the locale to a standard format.
+     */
+    public function normalizeLocale(string $locale): string
+    {
+        if (str_contains($locale, '_')) {
+            $parts = explode('_', $locale);
+
+            if (count($parts) >= 2) {
+                return sprintf('%s_%s', strtolower($parts[0]), strtoupper($parts[1]));
+            }
+        }
+
+        return strtolower($locale);
+    }
+
+    /**
      * Return all the translations.
      */
     public function getAllTranslations(): array
@@ -51,18 +67,19 @@ class LaravelTranslationsSync
      */
     public function getTranslationsForLocale(string $locale): array
     {
+        $normalizedLocale = $this->normalizeLocale($locale);
         $strings = [];
 
         // Load all translation files from the locale's directory.
-        if (File::exists(lang_path($locale))) {
-            foreach (File::files(lang_path($locale)) as $file) {
+        if (File::exists(lang_path($normalizedLocale))) {
+            foreach (File::files(lang_path($normalizedLocale)) as $file) {
                 $name = basename($file);
                 $strings[$name] = require $file;
                 ksort($strings[$name], SORT_STRING | SORT_FLAG_CASE);
             }
         }
 
-        $jsonPath = lang_path("$locale.json");
+        $jsonPath = lang_path("$normalizedLocale.json");
         if (File::exists($jsonPath)) {
             $json = File::get($jsonPath);
             $strings['json'] = json_decode($json, true, flags: JSON_THROW_ON_ERROR);
